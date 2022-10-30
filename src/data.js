7309e126a7c5f0ee4f25a3f06d65c1af23a0cc8a
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-30 21:52:48 +0100

    chore:added data as json

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const linkData = [
+  {
+    id: "",
+    urlLink: "https://twitter.zuri.team",
+    btnText: "Twitter Link",
+  },
+  {
+    id: "btn__zuri",
+    urlLink: "https://training.zuri.team",
+    btnText: "Zuri Team",
+  },
+  {
+    id: "books",
+    urlLink: "https://books.zuri.team",
+    btnText: "Zuri Books",
+  },
+  {
+    id: "book__python",
+    urlLink: "https://books.zuri.team",
+    btnText: "Python  Books",
+  },
+  {
+    id: "pitch",
+    urlLink: "https://background.zuri.team",
+    btnText: "Background Check for Coders",
+  },
+  {
+    id: "book__design",
+    urlLink: "https://background.zuri.team",
+    btnText: "Design Books",
+  },
+];
+
+export default linkData;
